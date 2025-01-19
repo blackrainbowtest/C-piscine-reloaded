86785b6d260a84c25728c3509f2c011022ae38c9
aramarak <aramarak@student.42yerevan.am>
2025-01-19 15:37:55 +0400

    add: ex13

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -1,26 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/19 15:21:15 by aramarak          #+#    #+#             */
-/*   Updated: 2025/01/19 15:35:53 by aramarak         ###   ########.fr       */
+/*   Created: 2025/01/19 15:29:45 by aramarak          #+#    #+#             */
+/*   Updated: 2025/01/19 15:37:30 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_iterative_factorial(int nb)
+int	ft_recursive_factorial(int nb)
 {
-	int		fact;
-
-	fact = 1;
 	if (nb < 0 || nb > 12)
 		return (0);
-	while (nb > 1)
-	{
-		fact *= nb;
-		nb -= 1;
-	}
-	return (fact);
+	if (nb == 1 || nb == 0)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
 }
