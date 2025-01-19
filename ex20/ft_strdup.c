e0647b1b993d7b1d900b92647ec8c292ac50b2d2
aramarak <aramarak@student.42yerevan.am>
2025-01-19 17:18:34 +0400

    add

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/19 17:02:55 by aramarak          #+#    #+#             */
+/*   Updated: 2025/01/19 17:17:30 by aramarak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char	*str;
+	int		size;
+	int		i;
+
+	i = 0;
+	size = 0;
+	while (src[size])
+		size++;
+	str = (char *)malloc((size + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	while (src[i])
+	{
+		str[i] = src[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
